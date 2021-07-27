3f256a80f2cef52805849e866bbc4502c5a93479
yoyoyeah <harrisin2037@gmail.com>
2021-07-27 13:29:11 +0800

    update

--- src/main/java/com/example/demo/interview/dao/UserDaoImpl.java
+++ src/main/java/com/example/demo/interview/dao/UserDaoImpl.java
@@ -1,10 +1,8 @@
 package com.example.demo.interview.dao;
 
-import java.math.BigInteger;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
-import java.util.UUID;
 
 import javax.annotation.PostConstruct;
 import javax.sql.DataSource;
